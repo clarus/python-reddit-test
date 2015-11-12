02d8a7b6dc1420373369c3428fe2dd1d7072eb1b
Guillaume Claret <dev@clarus.me>
2015-11-12 19:29:26 +0100

    Download the pictures but in the wrong format

--- download.py
+++ download.py
@@ -1,6 +1,11 @@
 import praw
+import urllib.request
 
 r = praw.Reddit(user_agent="clarus_tester")
 submissions = r.get_subreddit("aww").get_hot(limit=5)
-for x in submissions:
-    print(x.score, x.url)
+for submission in submissions:
+    print("Downloading", submission.title)
+    picture = urllib.request.urlopen(submission.url).read()
+    output = open("pictures/" + str(submission.score) + ".jpg", "wb")
+    output.write(picture)
+    output.close()
